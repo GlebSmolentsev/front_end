d4e5344428820f07dfb07e656c753475a3bda95f
GlebSmoletsev <smolentseva.tati@yandex.ru>
2023-12-22 20:40:40 +0300

    упращение works

--- js/training.js
+++ js/training.js
@@ -1,9 +1,18 @@
 console.log('Training')
 
-let ipo = 0
-for ( let i = 0; i < 6; i++){
-    // ipo = ipo + 1
-    // ipo += 1
-    // ipo++
-}
- console.log(ipo)
+// let ipo = 0
+// for ( let i = 0; i < 6; i++){
+//     // ipo = ipo + 1
+//     // ipo += 1
+//     // ipo++
+// }
+//  console.log(ipo)
+
+//  let lom = [1, 2, 'Привет', true, 0, 5,  'что-то', 'Hello']
+//  let i = 2
+//  console.log(lom[i])
+//  for ( let i = 0; i < 8; i++) {
+//     if (lom[i] === 'Hello') {
+//         alert('Слово Hello есть в массиве')
+//     }
+//     }
