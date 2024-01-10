caf8ca098a188d12749a7a77d39018d863b0794f
GlebSmoletsev <smolentseva.tati@yandex.ru>
2024-01-10 21:40:02 +0300

    добавили js в contacts

--- js/script.js
+++ js/script.js
@@ -1,23 +1,58 @@
-// console.log('Script JS');
-// https://glebsmolentsev.github.io/front_end/
-
-// console.log(5+5)
-// console.log("abc")
-
-let myName = 'Gleb'
-// console.log(myName);
 
 
-let num1 = 10
-let num2 = 15
 
-let res = num1 + num2
+let inputName = document.querySelector('.input-name')
 
-// console.log(res);
+console.log(inputName)
 
 
-// function sum(a,b) {
-//     return a + b
-// }
-// console.log( sum(10,500))
+let inputEmail = document.querySelector('.input-email')
+
+let formButton = document.querySelector('.form-button')
+
+
+
+// let inputNameValue = document.querySelector('.input-name-value')
+
+
+let userName = null
+
+inputName.addEventListener('input', function (event) {
+	console.log(event)
+	// userName = event.target.value
+	// console.log(userName)
+})
+
+let userEmail = null
+
+inputEmail.addEventListener('input', function(event) {
+    userEmail = event.target.value
+	console.log(userEmail)
+})
+
+
+
+formButton.addEventListener('click', function () {
+
+	console.log(userName, 'Name')
+	console.log(userEmail, 'Email')
+
+})
+
+console.log(userName, 'Name')
+console.log(userEmail, 'Email')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
